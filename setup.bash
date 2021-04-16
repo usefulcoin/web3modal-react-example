58715e3b8c870f80be69ce4d2c73b381b08ac57a
munair <munair@gmail.com>
2021-04-16 09:49:47 -0500

    swap out npm with yarn for installing web3

--- setup.bash
+++ setup.bash
@@ -15,13 +15,15 @@ sudo apt -y install unzip > /dev/null 2>&1 && echo [$0] installed unzip APT.
 sudo apt -y install npm > /dev/null 2>&1 && echo [$0] installed NPM APT.
 sudo apt -y install vim > /dev/null 2>&1 && echo [$0] installed vim APT.
 
-# step 2: install Web3Modal React Example.
-if $debug ; then sudo npm install ; fi
-sudo npm install > /dev/null 2>&1 && echo [$0] installed Express Generator.
+# step 2: install Yarn.
+if $debug ; then sudo npm install --global yarn; fi
+sudo npm install --global yarn > /dev/null 2>&1 && echo [$0] installed Yarn.
 
 # step 3: verify the installation of APTs.
 nodeversion=$(nodejs --version) && echo [$0] verified the installation of nodejs version $nodeversion.
 npmversion=$(npm --version) && echo [$0] verified the installation of npm version $npmversion.
+yarnversion=$(yarn --version) && echo [$0] verified the installation of yarn version $yarnversion.
 
-# step 4: start React app.
-npm start
+# step 4: install Web3Modal React Example.
+if $debug ; then sudo yarn install ; fi
+sudo yarn install > /dev/null 2>&1 && echo [$0] installed Express Generator.
